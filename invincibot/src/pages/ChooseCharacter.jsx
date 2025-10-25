cb78d7f9e2bc74506ec79526d2d14ea16af63ca7
Gabbie <gabbiecoronel@gmail.com>
2025-10-25 05:16:25 -0400

    Background image for Choose Character Page

--- invincibot/src/pages/ChooseCharacter.jsx
+++ invincibot/src/pages/ChooseCharacter.jsx
@@ -1,17 +1,35 @@
-import backgroundImage from '../assets/Choose-Your-Character.jpg';
-import '../pages/ChooseCharacter.css';
-import "../App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 export default function ChooseCharacter() {
   return (
-    <div className="choose-character flex flex-col items-center justify-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className="title"> Invincibot </div>
-      <div className="subtitle"> Choose Your Character </div>
-      <div className="character-options">
-        <button className="character-button">Warrior</button>
-        <button className="character-button">Mage</button>
-        <button className="character-button">Rogue</button>
-      </div>
-    </div>
-  )
+    <>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="relative h-screen w-full bg-[url('assets/img/Choose-Your-Character.jpg')] bg-cover bg-center">
+              {/* Black overlay */}
+              <div className="absolute inset-0 bg-black/30"></div>
+
+              {/* Content above overlay */}
+              <div className="relative flex flex-col items-center justify-start h-full pt-6 text-center text-white">
+                <h1 className="text-8xl font-bold font-title mb-3">
+                 Invincibot
+                </h1>
+                <p className="font-title text-3xl mb-90 font-light">Choose Your Character</p>
+                <button
+                  onClick={() => navigate("/choosecharacter")}
+                  className="cursor-pointer bg-blue-500 text-yellow-200 text-xl px-6 py-2 rounded hover:bg-blue-600 hover:shadow-[0_0_30px_10px_#3b82f6] transition-shadow duration-300"
+                >
+                  Let's Get Started
+                </button>
+              </div>
+            </div>
+            }
+          />
+          <Route path="/choosecharacter" element={<ChooseCharacter />} />
+        </Routes>
+
+    </>
+  );
 }
