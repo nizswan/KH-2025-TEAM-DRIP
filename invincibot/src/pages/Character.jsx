b8f921185537d71682c8abefe137a47830ea54c2
Gabbie <gabbiecoronel@gmail.com>
2025-10-25 14:08:06 -0400

    Character pages are set up for each character

---
+++ invincibot/src/pages/Character.jsx
@@ -0,0 +1,29 @@
+
+import { useLocation, useParams } from "react-router-dom";
+
+export default function CharacterPage() {
+    const { name } = useParams();
+    const location = useLocation();
+    const background = location.state?.background;
+    const pronoun = location.state?.pronoun;
+
+    return (
+        <div
+            className="h-screen w-full bg-cover bg-center text-white flex items-center justify-center"
+            style={{
+                backgroundImage: `url(${background})`,
+            }}
+
+        >
+            <div className="absolute inset-0 bg-black/30">
+                <div className="relative flex flex-col items-center justify-start h-full pt-6 text-center text-white">
+                    <h1 className="text-8xl font-bold font-title mb-3">
+                        Invincibot
+                    </h1>
+                    <p className="font-title text-3xl mb-90 font-light">Chat with {name} and {pronoun} allies to recieve summaries of materials you share</p>
+                </div>
+            </div>
+
+        </div>
+    );
+}
--- invincibot/src/pages/ChooseCharacter.jsx
+++ invincibot/src/pages/ChooseCharacter.jsx
@@ -3,17 +3,27 @@ import React, { useState } from "react";
 import invincibleImg from "../assets/img/Invincible.png";
 import SamanthaImg from "../assets/img/Samantha.png";
 import OmniManImg from "../assets/img/OmniMan.png";
+import invincibleImgBackground from "../assets/img/InvincibleBackground.png";
+import SamanthaImgBackground from "../assets/img/SamanthaBackground.png";
+import OmniManImgBackground from "../assets/img/OmniManBackground.png";
+import Character from "../pages/Character";
 
 export default function ChooseCharacter() {
   const navigate = useNavigate();
 
   const characters = [
-    { name: "Invincible", image: invincibleImg },
-    { name: "Samantha", image: SamanthaImg },
-    { name: "OmniMan", image: OmniManImg },
+    { name: "Invincible", image: invincibleImg, pronoun: "his" },
+    { name: "Samantha", image: SamanthaImg, pronoun: "her" },
+    { name: "OmniMan", image: OmniManImg, pronoun: "his" },
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(1); 
+  const characterBackground = [
+    { name: "Invincible", image: invincibleImgBackground },
+    { name: "Samantha", image: SamanthaImgBackground },
+    { name: "OmniMan", image: OmniManImgBackground },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(1);
 
   const nextCharacter = () =>
     setCurrentIndex((i) => (i + 1) % characters.length);
@@ -24,77 +34,74 @@ export default function ChooseCharacter() {
   const prev = characters[(currentIndex - 1 + characters.length) % characters.length];
   const next = characters[(currentIndex + 1) % characters.length];
 
+  const handleChoose = (character) => {
+    const selectedBackground = characterBackground.find(bg => bg.name === character.name);
+    navigate(`/character/${character.name}`, { state: { background: selectedBackground.image, pronoun: character.pronoun } });
+  };
+
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="relative h-screen w-full bg-[url('assets/img/Choose-Your-Character.jpg')] bg-cover bg-center">
-              {/* Black overlay */}
-              <div className="absolute inset-0 bg-black/30"></div>
+      <div className="relative h-screen w-full bg-[url('assets/img/Choose-Your-Character.jpg')] bg-cover bg-center">
+        {/* Black overlay */}
+        <div className="absolute inset-0 bg-black/30"></div>
 
-              {/* Content above overlay */}
-              <div className="relative flex flex-col items-center justify-start h-full pt-6 text-center text-white">
-                <h1 className="text-8xl font-bold font-title mb-3">Invincibot</h1>
-                <p className="font-title text-3xl mb-10 font-light text-white ">
-                  Choose Your Character
-                </p>
+        {/* Content above overlay */}
+        <div className="relative flex flex-col items-center justify-start h-full pt-6 text-center text-white">
+          <h1 className="text-8xl font-bold font-title mb-3">Invincibot</h1>
+          <p className="font-title text-3xl mb-10 font-light text-white ">
+            Choose Your Character
+          </p>
 
-                <div className="carousel flex items-center justify-center gap-6">
-                  <button className="nav-btn text-4xl" onClick={prevCharacter}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="107" height="107" viewBox="0 0 107 107" fill="none">
-                      <path d="M84.7084 53.5H22.2917M22.2917 53.5L53.5001 84.7083M22.2917 53.5L53.5001 22.2917" stroke="white" stroke-opacity="0.8" stroke-width="8.91667" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                  </button>
+          <div className="carousel flex items-center justify-center gap-6">
+            <button className="nav-btn text-4xl" onClick={prevCharacter}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="107" height="107" viewBox="0 0 107 107" fill="none">
+                <path d="M84.7084 53.5H22.2917M22.2917 53.5L53.5001 84.7083M22.2917 53.5L53.5001 22.2917" stroke="white" stroke-opacity="0.8" stroke-width="8.91667" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </button>
 
-                  {/* Left Character */}
-          <div className="opacity-60 scale-75 transition-all duration-300">
-            <img
-              src={prev.image}
-              alt={prev.name}
-              className="w-32 h-32 rounded-full border-4 border-white shadow-lg object-cover"
-            />
+            {/* Left Character */}
+            <div className="opacity-60 scale-75 transition-all duration-300">
+              <img
+                src={prev.image}
+                alt={prev.name}
+                className="w-32 h-32 rounded-full border-4 border-white shadow-lg object-cover"
+              />
+            </div>
+
+            {/* Center Character */}
+            <div className="flex flex-col items-center transition-all duration-300">
+              <img
+                src={currentCharacter.image}
+                alt={currentCharacter.name}
+                className="w-48 h-48 rounded-full border-4 border-white shadow-2xl object-cover "
+              />
+              <h2 className="character mt-4">{currentCharacter.name}</h2>
+            </div>
+
+            {/* Right Character */}
+            <div className="opacity-60 scale-75 transition-all duration-300">
+              <img
+                src={next.image}
+                alt={next.name}
+                className="w-32 h-32 rounded-full border-4 border-white shadow-lg object-cover"
+              />
+            </div>
+
+            <button className="nav-btn text-4xl" onClick={nextCharacter}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="106" height="106" viewBox="0 0 106 106" fill="none">
+                <path d="M22.0833 53H83.9166M83.9166 53L52.9999 22.0833M83.9166 53L52.9999 83.9167" stroke="#F5F5F5" stroke-width="8.83333" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </button>
           </div>
 
-          {/* Center Character */}
-          <div className="flex flex-col items-center transition-all duration-300">
-            <img
-              src={currentCharacter.image}
-              alt={currentCharacter.name}
-              className="w-48 h-48 rounded-full border-4 border-white shadow-2xl object-cover "
-                    />
-                    <h2 className="character mt-4">{currentCharacter.name}</h2>
-                  </div>
-
-                  {/* Right Character */}
-                  <div className="opacity-60 scale-75 transition-all duration-300">
-                    <img
-                      src={next.image}
-                      alt={next.name}
-                      className="w-32 h-32 rounded-full border-4 border-white shadow-lg object-cover"
-                    />
-                  </div>
-
-                  <button className="nav-btn text-4xl" onClick={nextCharacter}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="106" height="106" viewBox="0 0 106 106" fill="none">
-                      <path d="M22.0833 53H83.9166M83.9166 53L52.9999 22.0833M83.9166 53L52.9999 83.9167" stroke="#F5F5F5" stroke-width="8.83333" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                  </button>
-                </div>
-
-                <button
-                  onClick={() => navigate("/Character/" + currentCharacter.name)}
-                  className="mt-10 cursor-pointer bg-blue-500 text-yellow-200 text-xl px-6 py-2 rounded hover:bg-blue-600 hover:shadow-[0_0_30px_10px_#3b82f6] transition-shadow duration-300"
-                >
-                  Let&apos;s Get Started
-                </button>
-              </div>
-            </div>
-          }
-        />
-        <Route path="/Character/:name" element={<div>Character</div>} />
-      </Routes>
+          <button
+            onClick={() => handleChoose(currentCharacter)}
+            className="mt-10 cursor-pointer bg-blue-500 text-yellow-200 text-xl px-6 py-2 rounded hover:bg-blue-600 hover:shadow-[0_0_30px_10px_#3b82f6] transition-shadow duration-300"
+          >
+            Let&apos;s Get Started
+          </button>
+        </div>
+      </div>
     </>
   );
 }
